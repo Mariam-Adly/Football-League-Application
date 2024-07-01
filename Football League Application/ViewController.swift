36bc90a33e6e0e13410cf9b2dfc4fe2e4da806dd
Mariam Adly <mariamadly712@gmail.com>
2024-07-01 12:02:51 -0700

    Initial Commit

---
+++ Football League Application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Football League Application
+//
+//  Created by mariam adly on 01/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Football League ApplicationUITests/Football_League_ApplicationUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Football_League_ApplicationUITestsLaunchTests.swift
+//  Football League ApplicationUITests
+//
+//  Created by mariam adly on 01/07/2024.
+//
+
+import XCTest
+
+final class Football_League_ApplicationUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
